7d658f8dfd1482b1352ff9d9e85a7a8c668ed220
Kenan Rastoder <kenanrastodernp@gmail.com>
2026-04-23 22:41:55 +0200

    first-commit

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "API IS RUNNING",
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
