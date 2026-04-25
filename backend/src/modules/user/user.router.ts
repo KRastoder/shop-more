57b285795d1de0cda1571d79e0feb413e7d35c20
Kenan Rastoder <kenanrastodernp@gmail.com>
2026-04-25 02:33:47 +0200

    refactored to the better-auth libary

--- backend/src/modules/user/user.router.ts
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import { createUserController } from "./user.controller";
-
-const userRouter = Router();
-
-userRouter.post("/create", createUserController);
-
-export default userRouter;
