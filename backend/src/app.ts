81a0a3f2d8260edfc0a5726c0c9b53a568c2cf54
Kenan Rastoder <kenanrastodernp@gmail.com>
2026-04-25 15:45:41 +0200

    feature:Added auth middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,27 @@
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
-import userRouter from "./modules/user/user.router";
+import { auth } from "./auth/auth";
+import { requireAuth } from "./middleware/auth.middleware";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", //TODO change later
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.get("/test", requireAuth, (req, res) => {
+  return res.status(200).json({ fail: true });
+});
+
+app.use(express.json()); // must come after auth routes according to better auth docs
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "API IS RUNNING" });
 });
-
-app.use("/user", userRouter);
-
 export default app;
---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from "express";
+import { auth } from "../auth/auth";
+import { fromNodeHeaders } from "better-auth/node";
+
+export async function requireAuth(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+    if (!session) {
+      return res.status(401).json({ err: "Unothorized" });
+    }
+    next();
+  } catch (e) {
+    console.error(e);
+    return res.status(401).json({ error: "Unothorized" });
+  }
+}
